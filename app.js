8d653e785949511e2362d0b34798bc92735d2990
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-21 22:39:52 -0400

    add gallery js open modal

--- app.js
+++ app.js
@@ -7,3 +7,34 @@ function getElement(selection) {
     throw new Error(`please check selector:"${selection}" there is an error`);
   }
 }
+
+//constructor
+function Gallery(element) {
+  this.container = element;
+  this.list = [...element.querySelectorAll('.img')];
+  this.modal = getElement('.modal');
+  this.mainImage = getElement('.main-img');
+  this.imageName = getElement('.image-name');
+  this.modalImages = getElement('.modal-images');
+  this.closeBtn = getElement('.close-btn');
+  this.prevBtn = getElement('.prev-btn');
+  this.nextBtn = getElement('.next-btn');
+  this.container.addEventListener(
+    'click',
+    function (e) {
+      if (e.target.classList.contains('img')) {
+        this.openModal(e.target, this.list);
+      }
+    }.bind(this)
+  );
+}
+
+//proto
+Gallery.prototype.openModal = function (selected, list) {
+  console.log(selected, list);
+  this.modal.classList.add('open');
+};
+
+//instances
+const waves = new Gallery(getElement('.waves'));
+const space = new Gallery(getElement('.space'));
